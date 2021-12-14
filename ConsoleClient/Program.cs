e23a4fa6ddac1d89a64e0f13ff9526067cdc01cc
dsconsole283 <dsconsole_2@yahoo.com>
2021-12-14 11:56:57 -0800

    Adding clients

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Text.RegularExpressions;
+using static System.Console;
 
 namespace ConsoleClient
 {
     class Program
     {
+        public static HubConnection _connection = ConnectToServer(@"https://aa81-2601-548-4100-c1f0-4c6d-8e0-4cf0-7abf.ngrok.io/chat");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                _connection.StartAsync().Wait();
+            }
+            catch (AggregateException)
+            {
+                WriteLine($"The server is currently down. Please try again later.");
+                Write("\r\nPress enter to exit");
+                ReadKey();
+                Environment.Exit(0);
+            }
+            _connection.StartAsync().Wait();
+
+            AskForUserName();
+
+            do
+            {
+                SendMessage();
+
+            } while (_connection.State == HubConnectionState.Connected);
         }
+
+        private static HubConnection ConnectToServer(string address)
+        {
+            return new HubConnectionBuilder().WithUrl(address).WithAutomaticReconnect().Build();
+        }
+
+        private static void SendUserNameToServer(string userName)
+        {
+            _connection.InvokeCoreAsync("RetrieveUsername", args: new[] { userName });
+            _connection.On("ReceiveChatMessage", (string message) =>
+            {
+                WriteLine($"{ message }");
+            });
+        }
+
+        private static string AskForUserName()
+        {
+            string userInput = "";
+            bool isValidName = false;
+
+            do
+            {
+                Write("What would you like your user name to be?  ");
+                userInput = ReadLine();
+
+                string pattern = @"^[a-zA-Z0-9]+$";
+                Regex exp = new Regex(pattern);
+
+                if (exp.IsMatch(userInput))
+                    isValidName = true;
+
+                else
+                {
+                    WriteLine("Invalid, try again (no special characters)..");
+                    isValidName = false;
+                }
+            }
+            while (!isValidName);
+
+            SendUserNameToServer(userInput);
+
+            return userInput;
+        }
+
+        private static void SendMessage()
+        {
+            string message = ReadLine();
+            _connection.InvokeCoreAsync("SendChatMessage", args: new[] { message });
+        }
+
     }
 }
