e1b882d1d69f1fac82427c43566dfba25ee73419
Nick Whitley <nicrofilm@gmail.com>
2021-12-28 13:38:41 -0500

    added better dependency management and file processor. Need to redesign chathub and build out file saving

---
+++ WebServer/Interfaces/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServer.User
+{
+    public interface IUser
+    {
+        string connectionId { get; set; }
+        string password { get; set; }
+        string username { get; set; }
+    }
+}
